67595798963f51daf58eb82c550147e61b87af40
valerieponce <vponcelu@calpoly.edu>
2025-12-04 20:27:00 -0800

    test

--- packages/react-frontend/src/apiConfig.js
+++ packages/react-frontend/src/apiConfig.js
@@ -3,4 +3,4 @@ const isProd = import.meta.env.PROD;
 // In dev: talk to localhost backend.
 // In prod: use an environment variable from Azure.
 export const API_BASE_URL = 
-  import.meta.env.VITE_API_URL || "http://localhost:8000";
+  import.meta.env.VITE_API_URL;
