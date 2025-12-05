5f178a7eae9b1a5e71976b3387beec78d43c28ab
valerieponce <vponcelu@calpoly.edu>
2025-12-04 20:57:59 -0800

    maybe this time

--- packages/react-frontend/src/apiConfig.js
+++ packages/react-frontend/src/apiConfig.js
@@ -2,5 +2,5 @@ const isProd = import.meta.env.PROD;
 
 // In dev: talk to localhost backend.
 // In prod: use an environment variable from Azure.
-export const API_BASE_URL = 
+export const VITE_API_BASE_URL = 
   import.meta.env.VITE_API_URL;
